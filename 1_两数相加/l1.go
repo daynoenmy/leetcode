a39a3ca267fe6b983c19460b8d1e7fbafeb6b019
daynoenmy <intmain20204906@163.com>
2025-05-09 10:11:10 +0800

    5.9 10:11

--- 1_两数相加/l1.go
+++ 1_两数相加/l1.go
@@ -1,1 +1,16 @@
-package __两数相加
+package main
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		k, v := m[target-nums[i]]
+		if v {
+			return []int{i, k}
+		}
+		m[nums[i]] = i
+	}
+	return []int{0, 0}
+}
+func main() {
+
+}
--- 20_有效括号/g.go
+++ 20_有效括号/g.go
@@ -1,1 +1,73 @@
-package _0_有效括号
+package main
+
+type stack struct {
+	data []int32
+	p    int
+}
+
+func (s *stack) pop() (int32, bool) {
+	if s.p == 0 {
+		println("stack is empty")
+		return ' ', false
+	}
+	s.p -= 1
+	return s.data[s.p], true
+}
+func (s *stack) peek() (int32, bool) {
+	if s.p == 0 {
+		println("stack is empty")
+		return ' ', false
+	}
+	return s.data[s.p-1], true
+}
+func (s *stack) push(val int32) {
+	s.data[s.p] = val
+	s.p += 1
+}
+func (s *stack) init(len int) {
+	s.p = 0
+	s.data = make([]int32, len)
+}
+func isVaild(s string) bool {
+	st := new(stack)
+	st.init(len(s))
+	for _, v := range s {
+		if v == '(' || v == '[' || v == '{' {
+			st.push(v)
+		} else if v == ')' {
+			k, t := st.peek()
+			if !t {
+				return false
+			}
+			if k == '(' {
+				st.pop()
+			} else {
+				return false
+			}
+		} else if v == ']' {
+			k, t := st.peek()
+			if !t {
+				return false
+			}
+			if k == '[' {
+				st.pop()
+			} else {
+				return false
+			}
+		} else if v == '}' {
+			k, t := st.peek()
+			if !t {
+				return false
+			}
+			if k == '{' {
+				st.pop()
+			} else {
+				return false
+			}
+		}
+	}
+	return st.p == 0
+}
+func main() {
+	println(isVaild("(()[{}])"))
+}
